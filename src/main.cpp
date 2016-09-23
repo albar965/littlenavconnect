b5b45c0c713cab5370f974ef1558d5649a3f7a99
Alexander Barthel <albar965@mailbox.org>
2016-09-23 21:39:28 +0200

    Updated version number to 1.0.0.beta

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
   Application::setApplicationName("Little Navconnect");
   Application::setOrganizationName("ABarthel");
   Application::setOrganizationDomain("abarthel.org");
-  Application::setApplicationVersion("0.9.9.develop");
+  Application::setApplicationVersion("1.0.0.beta");
 
   // Initialize logging and force logfiles into the system or user temp directory
   LoggingHandler::initializeForTemp(atools::settings::Settings::getOverloadedPath(
