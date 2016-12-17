9cc61c3f64558629f77fa83c80c8d62bf3868c8b
Alexander Barthel <albar965@mailbox.org>
2016-12-17 20:46:44 +0100

    Version 1.2.0.beta

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
   Application::setApplicationName("Little Navconnect");
   Application::setOrganizationName("ABarthel");
   Application::setOrganizationDomain("abarthel.org");
-  Application::setApplicationVersion("1.1.0.devel");
+  Application::setApplicationVersion("1.2.0.beta");
   Application::setEmailAddresses({"albar965@mailbox.org", "albar965@t-online.de"});
 
   // Initialize logging and force logfiles into the system or user temp directory
