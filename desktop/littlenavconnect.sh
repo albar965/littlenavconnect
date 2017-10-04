e839054d1f068a333635204c8b4900e2781ed6e2
Alexander Barthel <albar965@mailbox.org>
2017-10-04 15:29:31 +0200

    Fixed linux startup script to keep LD_LIBRARY_PATH local and added comment.

--- desktop/littlenavconnect.sh
+++ desktop/littlenavconnect.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=./lib
+# Adjust this script like shown below and remove "LD_LIBRARY_PATH=./lib" if you want to move it to another place
+# cd "/home/YOURUSERNAME/Little Navconnect"
+# LD_LIBRARY_PATH="/home/YOURUSERNAME/Little Navconnect/lib"
+
+LD_LIBRARY_PATH=./lib
 
 ./littlenavconnect
