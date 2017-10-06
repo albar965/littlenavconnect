eda46b08a84b8e4b3c220c674e5132b059779152
Alexander Barthel <albar965@mailbox.org>
2017-10-07 00:11:45 +0200

    Updated linux starup script.

--- desktop/littlenavconnect.sh
+++ desktop/littlenavconnect.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Adjust this script like shown below and remove "LD_LIBRARY_PATH=./lib" if you want to move it to another place
+# or if you use a symbolic link to this script
+# Keep the quotation marks if you path contains spaces
 # cd "/home/YOURUSERNAME/Little Navconnect"
 # LD_LIBRARY_PATH="/home/YOURUSERNAME/Little Navconnect/lib"
 
-# Use subshell to keep LD_LIBRARY_PATH local
-(
 export LD_LIBRARY_PATH=./lib
+
 ./littlenavconnect
-)
