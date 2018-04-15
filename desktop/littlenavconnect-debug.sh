c3dbffb859c2d5dc3458020afcd84991859596f8
Alexander Barthel <albar965@mailbox.org>
2018-04-15 13:57:21 +0200

    Qt 5.9.5

--- desktop/littlenavconnect-debug.sh
+++ desktop/littlenavconnect-debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=~/Qt/5.9.1/gcc_64/lib
+export LD_LIBRARY_PATH=~/Qt/5.9.5/gcc_64/lib
 
 cd ~/Projekte/build-littlenavconnect-debug
 ~/Projekte/build-littlenavconnect-debug/littlenavconnect -g
