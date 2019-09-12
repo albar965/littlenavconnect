34880ef2220253ba1f6c573312ca390c4d656068
Alexander Barthel <albar965@mailbox.org>
2019-09-12 15:34:40 +0200

    Qt 5.12.5

--- desktop/littlenavconnect-debug.sh
+++ desktop/littlenavconnect-debug.sh
@@ -6,6 +6,6 @@ CONF=debug
 
 cd ${APROJECTS}/build-littlenavconnect-${CONF}
 
-export LD_LIBRARY_PATH=~/Qt/5.12.4/gcc_64/lib:${APROJECTS}/build-littlenavconnect-${CONF}
+export LD_LIBRARY_PATH=~/Qt/5.12.5/gcc_64/lib:${APROJECTS}/build-littlenavconnect-${CONF}
 
 ${APROJECTS}/build-littlenavconnect-${CONF}/littlenavconnect --replay-gui $@
