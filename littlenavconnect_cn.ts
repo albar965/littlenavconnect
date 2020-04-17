51921c21c86807bf32fec4c05da702f45002e24e
John Liu <johnliu@johnliu.info>
2020-04-17 14:49:51 +0800

    completed Chinese translation

--- littlenavconnect_cn.ts
+++ littlenavconnect_cn.ts
@@ -1,187 +1,189 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="zh">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
         <source>Little Navconnect</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="50"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>文件(&amp;F)</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="56"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>帮助(&amp;H)</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="66"/>
         <source>&amp;Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>工具(&amp;T)</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="77"/>
         <source>Toolbar</source>
-        <translation type="unfinished"></translation>
+        <translation>工具栏</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="100"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>退出(&amp;Q)</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="103"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="112"/>
         <source>&amp;About Little Navconnect</source>
-        <translation type="unfinished"></translation>
+        <translation>关于Little Navconnect（&amp;A）</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="121"/>
         <source>About &amp;Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>关于 &amp;Qt</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="130"/>
         <source>&amp;Contents (Online)</source>
-        <translation type="unfinished"></translation>
+        <translation>帮助内容（在线&amp;C）</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="135"/>
         <source>&amp;Reset Messages</source>
-        <translation type="unfinished"></translation>
+        <translation>重设消息（&amp;R）</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="144"/>
         <source>&amp;Options</source>
-        <translation type="unfinished"></translation>
+        <translation>选项（&amp;O）</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="149"/>
         <source>&amp;Save Replay File ...</source>
-        <translation type="unfinished"></translation>
+        <translation>保存回放文件（&amp;S） ...</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="154"/>
         <source>Open Replay &amp;File ...</source>
-        <translation type="unfinished"></translation>
+        <translation>打开回放文件（&amp;F） ...</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="157"/>
         <source>Open Replay File</source>
-        <translation type="unfinished"></translation>
+        <translation>打开回放文件</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="162"/>
         <location filename="mainwindow.ui" line="165"/>
         <source>Stop Saving or Loading of Replay</source>
-        <translation type="unfinished"></translation>
+        <translation>停止保存或载入重放</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="174"/>
         <source>&amp;Contents (Offline, PDF)</source>
-        <translation type="unfinished"></translation>
+        <translation>帮助内容（离线PDF&amp;C）</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="185"/>
         <source>FSX or Prepar3D</source>
-        <translation type="unfinished"></translation>
+        <translation>FSX 或 Prepar3D</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="188"/>
         <source>Connect to FSX or Prepar3D using SimConnect.</source>
-        <translation type="unfinished"></translation>
+        <translation>使用SimConnect连接到FSX 或 Prepar3D。</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="191"/>
         <source>Ctrl+Shift+F</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="199"/>
         <source>X-Plane</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="202"/>
         <source>Connect to X-Plane using the Little Xpconnect plugin.</source>
-        <translation type="unfinished"></translation>
+        <translation>使用Xpconnect连接到X-Plane。</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="205"/>
         <source>Ctrl+Shift+X</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="249"/>
         <source>Connecting to FSX or Prepar3D using SimConnect.</source>
-        <translation type="unfinished"></translation>
+        <translation>正在使用SimConnect连接到FSX 或 Prepar3D。</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="255"/>
         <source>Connecting to X-Plane using the Little Xpconnect plugin.</source>
-        <translation type="unfinished"></translation>
+        <translation>正在使用Xpconnect连接到X-Plane。</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="273"/>
         <source>Save Replay</source>
-        <translation type="unfinished"></translation>
+        <translation>保存回放</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="273"/>
         <location filename="src/mainwindow.cpp" line="289"/>
         <source>Replay Files (*.replay);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>回放文举 (*.replay);;所有文件 (*)</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="289"/>
         <source>Open Replay</source>
-        <translation type="unfinished"></translation>
+        <translation>打开回放</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="365"/>
         <source>There are still applications connected.
 Really change the Network Port?</source>
-        <translation type="unfinished"></translation>
+        <translation>仍然有程序已连接。
+真的要改变网络端口号吗？</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="368"/>
         <location filename="src/mainwindow.cpp" line="458"/>
         <source>Do not &amp;show this dialog again.</source>
-        <translation type="unfinished"></translation>
+        <translation>不要再显示这个对话框(&amp;S)。</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="456"/>
         <source>There are still applications connected.
 Really Quit?</source>
-        <translation type="unfinished"></translation>
+        <translation>仍然有程序已连接。
+真的要退出吗？</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="476"/>
         <source>Version %1 (revision %2).</source>
-        <translation type="unfinished"></translation>
+        <translation>版本 %1 (修订 %2)。</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="480"/>
         <source>Data Version %1. Reply Version %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>数据 %1。回放版本 %2。</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="545"/>
         <source>Starting server. This can take up to a minute ...</source>
-        <translation type="unfinished"></translation>
+        <translation>正在启动服务器。这可能需要最多一分钟 ...</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="557"/>
         <source>Server running.</source>
-        <translation type="unfinished"></translation>
+        <translation>服务器已在运行。</translation>
     </message>
 </context>
 <context>
@@ -189,83 +191,86 @@ Really Quit?</source>
     <message>
         <location filename="optionsdialog.ui" line="14"/>
         <source>Little Navconnect - Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Little Navconnect - 选项</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="22"/>
         <location filename="optionsdialog.ui" line="25"/>
         <source>Deselect to disable fetching and transmission of AI or multiplayer aircraft</source>
-        <translation type="unfinished"></translation>
+        <translation>取消选中以禁止获取及传输AI或多人游戏航空器</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="28"/>
         <source>Fetch AI or multiplayer &amp;aircraft</source>
-        <translation type="unfinished"></translation>
+        <translation>获取及传输AI或多人游戏航空器（&amp;A）</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="38"/>
         <source>Allowed range is 50 milliseconds up to 2 seconds. 
 Little Navconnect fetches data from Flight Simulator and sends it to all clients (i.e. Little Navmap) using this time interval.</source>
-        <translation type="unfinished"></translation>
+        <translation>允许的范围是50毫秒到2秒。
+Little Navconnect 每隔相应的时间会向模拟器获取数据并发送到所有的客户端（例如Little Navmap）。</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="42"/>
         <source>Allowed range is 50 milliseconds up to 2 seconds.  Little Navconnect fetches data from Flight Simulator and sends it to all clients (i.e. Little Navmap) using this time interval.</source>
-        <translation type="unfinished"></translation>
+        <translation>允许的范围是50毫秒到2秒。Little Navconnect 每隔相应的时间会向模拟器获取数据并发送到所有的客户端（例如Little Navmap）。</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="45"/>
         <source> ms</source>
-        <translation type="unfinished"></translation>
+        <translation> 毫秒</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="64"/>
         <source>Select this if you do not want to reveal your hostname or IP address in the log window.
 This can be useful for streaming sessions where others can see your desktop.</source>
-        <translation type="unfinished"></translation>
+        <translation>如您不愿意日志窗口中显示您的主机名或IP地址可以选中此选项。
+这将会在有其他人可以看到您的桌面的在线直播会话中有用。</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="68"/>
         <source>Select this if you do not want to reveal your hostname or IP address in the log window.\nThis can be useful for streaming sessions where others can see your desktop.</source>
-        <translation type="unfinished"></translation>
+        <translation>如您不愿意日志窗口中显示您的主机名或IP地址可以选中此选项。这将会在有其他人可以看到您的桌面的在线直播会话中有用。</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="71"/>
         <source>&amp;Hide hostname and IP in log window</source>
-        <translation type="unfinished"></translation>
+        <translation>在日志窗口中隐藏主机名及IP地址（&amp;H）</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="78"/>
         <source>&amp;Network Port:</source>
-        <translation type="unfinished"></translation>
+        <translation>网络端口（&amp;N）：</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="91"/>
         <source>&amp;Update Time Interval:</source>
-        <translation type="unfinished"></translation>
+        <translation>更新时间间隔（&amp;U）：</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="104"/>
         <source>This can normally be left at default.
 Change this value only if another application occupies this port and 
 you get an error message like &quot;The bound address is already in use&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>这通常可以保留为默认值。
+只在有其他程序占用了此端口并且您遇到了错误信息例如“绑定的地址已在使用中”时更改。</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="109"/>
         <source>This can normally be left at default.\nChange this value only if another application occupies this port and \nyou get an error message like &quot;The bound address is already in use&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>这通常可以保留为默认值。只在有其他程序占用了此端口并且您遇到了错误信息例如“绑定的地址已在使用中”时更改。</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="125"/>
         <location filename="optionsdialog.ui" line="128"/>
         <source>Deselect to disable fetching and transmission of AI or multiplayer ships. This option does not apply to X-Plane.</source>
-        <translation type="unfinished"></translation>
+        <translation>取消选中以禁止获取及传输AI或多人游戏航船舶。这个选色不适用与X-Plane。</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="131"/>
         <source>Fetch AI or multiplayer &amp;ships (not X-Plane)</source>
-        <translation type="unfinished"></translation>
+        <translation>获取及传输AI或多人游戏船舶-不适用与X-Plane（&amp;S）</translation>
     </message>
 </context>
 <context>
@@ -273,58 +278,58 @@ you get an error message like &quot;The bound address is already in use&quot;.</
     <message>
         <location filename="src/main.cpp" line="70"/>
         <source>Log files:</source>
-        <translation type="unfinished"></translation>
+        <translation>日志文件：</translation>
     </message>
     <message>
         <location filename="src/main.cpp" line="72"/>
         <source>Configuration:</source>
-        <translation type="unfinished"></translation>
+        <translation>配置：</translation>
     </message>
     <message>
         <location filename="src/main.cpp" line="96"/>
         <source>%1 - Error</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 - 错误</translation>
     </message>
     <message>
         <location filename="src/main.cpp" line="97"/>
         <source>%1 is already running.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 已在运行。</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="64"/>
         <source>&lt;p&gt;is the Fligh Simulator Network agent for Little Navmap.&lt;/p&gt;&lt;p&gt;This software is licensed under &lt;a href=&quot;http://www.gnu.org/licenses/gpl-3.0&quot;&gt;GPL3&lt;/a&gt; or any later version.&lt;/p&gt;&lt;p&gt;The source code for this application is available at &lt;a href=&quot;https://github.com/albar965&quot;&gt;Github&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;More about my projects at &lt;a href=&quot;https://www.littlenavmap.org&quot;&gt;www.littlenavmap.org&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;&lt;b&gt;Copyright 2015-2020 Alexander Barthel&lt;/b&gt;&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;是Little Navmap的飞行模拟器网络代理&lt;/p&gt;&lt;p&gt;这个软件由&lt;a href=&quot;http://www.gnu.org/licenses/gpl-3.0&quot;&gt;GPL3&lt;/a&gt; 或更新版本授权。&lt;/p&gt;&lt;p&gt;此程序的源代码在 &lt;a href=&quot;https://github.com/albar965&quot;&gt;Github&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;更多关于我的项目： &lt;a href=&quot;https://www.littlenavmap.org&quot;&gt;www.littlenavmap.org&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;&lt;b&gt;Copyright 2015-2020 Alexander Barthel&lt;/b&gt;&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="97"/>
         <source>Save replay data to &lt;file&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>保存回放到 &lt;文件&gt;。</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="98"/>
         <location filename="src/mainwindow.cpp" line="103"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>文件</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="102"/>
         <source>Load replay data from &lt;file&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>自 &lt;文件&gt;载入回放。</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="107"/>
         <source>Use speed factor &lt;speed&gt; for replay.</source>
-        <translation type="unfinished"></translation>
+        <translation>使用倍数&lt;speed&gt;回放。</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="108"/>
         <source>speed</source>
-        <translation type="unfinished"></translation>
+        <translation>速度</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="112"/>
         <source>Show replay menu items.</source>
-        <translation type="unfinished"></translation>
+        <translation>显示回放目录项。</translation>
     </message>
 </context>
 </TS>
