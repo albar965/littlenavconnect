4b40a5dcad8b55feb50ead7e7907c7fadf3a12b1
Alexander Barthel <albar965@mailbox.org>
2025-02-05 23:33:05 +0100

    Updated copyright year in about

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright 2015-2024 Alexander Barthel alex@littlenavmap.org
+* Copyright 2015-2025 Alexander Barthel alex@littlenavmap.org
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -78,7 +78,7 @@ MainWindow::MainWindow()
                         "<a href=\"https://github.com/albar965\">GitHub</a>.</p>"
                           "<p>More about my projects at "
                             "<a href=\"https://www.littlenavmap.org\">www.littlenavmap.org</a>.</p>"
-                              "<p><b>Copyright 2015-2024 Alexander Barthel</b></p>");
+                              "<p><b>Copyright 2015-2025 Alexander Barthel</b></p>");
 
   // Show a dialog on fatal log events like asserts
   atools::logging::LoggingGuiAbortHandler::setGuiAbortFunction(this);
